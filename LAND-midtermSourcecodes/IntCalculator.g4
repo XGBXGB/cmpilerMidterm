612f35e6ff5694cba81dad9cef8659123df4de13
XGBXGB <kewpee10@hotmail.com>
2016-03-15 18:30:49 +0800

    Edited .g4 file, although hindi pa kumpleto yung performop lexer rules

--- LAND-midtermSourcecodes/IntCalculator.g4
+++ LAND-midtermSourcecodes/IntCalculator.g4
@@ -1,8 +1,15 @@
 grammar IntCalculator;
  
-// Parser Rules
+/*
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ */
  
-
  @lexer::members{
  	@Override
  	public void recover(LexerNoViableAltException e){
@@ -12,9 +19,59 @@ grammar IntCalculator;
  		throw new RuntimeException("Lexical error! Unable to compute for the answer.\n");
  	}	
  }
- 							
-perform_op returns [int value] :    
-								n1=perform_op n2=perform_op2 
+ 
+ //Data types and literals
+data_type: INT_DATA_TYPE | FLOAT_DATA_TYPE | STRING_DATA_TYPE | CHAR_DATA_TYPE;	
+literal: digit_literal | characters_literal;
+digit_literal: INT_LIT | FLOAT_LIT;
+characters_literal: STRING_LIT | CHAR_LIT;
+return_type: data_type | VOID_DATA_TYPE;
+ 	
+//Constant declaration
+constant_declaration: CONSTANT_TOKEN IDENTIFIER literal TERMINATOR_TOKEN;
+
+//Variable declaration/initialization
+var: IDENTIFIER | array;
+variable_declaration: data_type var more_variable_declaration | data_type assignment_declaration more_variable_declaration;
+more_variable_declaration: COMMA_TOKEN var more_variable_declaration | COMMA_TOKEN assignment_declaration more_variable_declaration | TERMINATOR_TOKEN;
+
+//Arrays
+array: IDENTIFIER OPEN_BRACKET expression CLOSE_BRACKET;
+
+//Assignment statement
+assignment_declaration: assignment | assignment_array;
+assignment_array: array ASSIGNMENT_OPERATOR OPEN_BRACE expression more_expressions CLOSE_BRACE;
+assignment: var ASSIGNMENT_OPERATOR expression;
+assignment_line: var ASSIGNMENT_OPERATOR expression TERMINATOR_TOKEN;
+
+//Function declaration
+function_declaration: return_type IDENTIFIER OPEN_PARENTHESIS function_declaration_parameters_type CLOSE_PARENTHESIS OPEN_BRACE code_block CLOSE_BRACE;
+function_declaration_parameters_type: function_declaration_parameters | NULL_TOKEN;
+function_declaration_parameters: return_type IDENTIFIER function_declaration_more_parameters;
+function_declaration_more_parameters: COMMA_TOKEN function_declaration_parameters function_declaration_more_parameters | NULL_TOKEN;
+
+//Function call
+function_call: IDENTIFIER OPEN_PARENTHESIS function_call_parameters_type CLOSE_PARENTHESIS;
+function_call_parameters_type: function_call_parameters | NULL_TOKEN;
+function_call_parameters: expression more_expressions;
+function_call_line: function_call TERMINATOR_TOKEN;
+
+//Conditional
+conditional: IF_TOKEN OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACE code_block CLOSE_BRACE conditional_continue;
+conditional_continue: ELSE_TOKEN conditional | ELSE_TOKEN OPEN_BRACE code_block CLOSE_BRACE;
+
+//Event-controlled loop
+wloop: WHILE_TOKEN OPEN_PARENTHESIS expression CLOSE_PARENTHESIS OPEN_BRACE code_block CLOSE_BRACE;
+
+//Repeat-until loop
+dloop: DO_TOKEN OPEN_BRACE code_block CLOSE_BRACE WHILE_TOKEN OPEN_PARENTHESIS expression CLOSE_PARENTHESIS TERMINATOR_TOKEN;
+
+//Count-controlled loop
+floop: FOR_TOKEN OPEN_PARENTHESIS expression TERMINATOR_TOKEN expression TERMINATOR_TOKEN expression CLOSE_PARENTHESIS OPEN_BRACE code_block CLOSE_BRACE;
+
+
+//Perform operation	rules					
+perform_op returns [int value] : n1=perform_op n2=perform_op2 
 							   { 
 								if($n2.text.startsWith("+") || $n2.text.startsWith("-"))
 								{
@@ -81,8 +138,7 @@ perform_op2 returns [int value]: n1=perform_op2 op=mul_div n2=perform_op3
 									}
 							    ;
 
-perform_op3 returns [int value]:
-								'(' c=perform_op ')'
+perform_op3 returns [int value]:'(' c=perform_op ')'
 							    {
 									$value = $c.value;
 							    }
@@ -93,12 +149,106 @@ perform_op3 returns [int value]:
 							    }
 
 								;
+add_sub: ADDITION_OPERATOR | SUBTRACTION_OPERATOR;
+mul_div: MULTIPLICATION_OPERATOR | DIVISION_OPERATOR | MODULUS_OPERATOR;
+cond_op: NOT_EQUAL_TO_OPERATOR | EQUAL_TO_OPERATOR | GREATER_THAN_OPERATOR | LESS_THAN_OPERATOR | GREATER_THAN_EQUAL_TO_OPERATOR | LESS_THAN_EQUAL_TO_OPERATOR;
 
-add_sub: '+' | '-';
-mul_div: '*' | '/' | '%';
+
+//Expression
+expression: IDENTIFIER | literal | function_call | perform_op | assignment | NULL_TOKEN;
+more_expressions: COMMA_TOKEN expression more_expressions | NULL_TOKEN;
+
+//Code
+code_block: variable_declaration code_block | assignment_line code_block | function_declaration code_block | function_call_line code_block | conditional code_block | wloop code_block | floop code_block | dloop code_block | NULL_TOKEN;
+/*
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ * PARSER PARSER PARSER PARSER PARSER PARSER PARSER RULES RULES RULES RULES RULES RULES RULES
+ */
 
 
-// Lexer Rules
+/*
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ */
+
+//Literals
 INT_LIT: (('+'|'-')?('0'..'9')+);
+FLOAT_LIT: (('+'|'-')?('0'..'9')*.?('0'..'9')+);//
+STRING_LIT: '\''('A'..'Z''a'..'z''0'..'9')*'\'';
+CHAR_LIT: '\"'('A'..'Z''a'..'z''0'..'9')'\"';
+IDENTIFIER: ('A'..'Z')('A'..'Z''a'..'z''0'..'9''_')*;
+
+//Data types
+INT_DATA_TYPE: 'float';
+FLOAT_DATA_TYPE: 'int';
+STRING_DATA_TYPE: 'char';
+CHAR_DATA_TYPE: 'string';
+VOID_DATA_TYPE: 'full';
+
+//Loops
+FOR_TOKEN: 'from';
+WHILE_TOKEN: 'thame';
+DO_TOKEN: 'dont';
+
+//Conditionals
+IF_TOKEN: 'then';
+ELSE_TOKEN: 'anne';
+RETURN_TOKEN: 'borrow';
+SINGLE_LINE_COMMENT_TOKEN: '*/'; 
+
+//Operators
+ASSIGNMENT_OPERATOR: ':';
+ADDITION_OPERATOR: '-';
+SUBTRACTION_OPERATOR: '+';
+MULTIPLICATION_OPERATOR: '/';
+DIVISION_OPERATOR: '*';
+MODULUS_OPERATOR: '%';
+INCREMENT_OPERATOR: '--';
+DECREMENT_OPERATOR: '++';
+AND_OPERATOR: '&&';
+OR_OPERATOR: '||';
+NOT_OPERATOR: ';';
+NOT_EQUAL_TO_OPERATOR: '::';
+EQUAL_TO_OPERATOR: ';:';
+GREATER_THAN_OPERATOR: ':<';
+LESS_THAN_OPERATOR: ':>';
+GREATER_THAN_EQUAL_TO_OPERATOR: '<';
+LESS_THAN_EQUAL_TO_OPERATOR: '>';
+
+//Misc
+NULL_TOKEN:'something';
+CONSTANT_TOKEN: 'changing';
+CONSTANT_DECLARATION: ('a'..'z')('a'..'z''_''0'..'9')*;
+TERMINATOR_TOKEN: '!';
+OPEN_PARENTHESIS: '(';
+CLOSE_PARENTHESIS: ')';
+OPEN_BRACE: '{';
+CLOSE_BRACE: '}';
+OPEN_BRACKET: '[';
+CLOSE_BRACKET: ']';
+COMMA_TOKEN: ',';
 WHITESPACE : ( '\t' |' ' | '\r' | '\n'| '\u000C'
 )+ -> skip ;
+
+/*
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ * LEXER LEXER LEXER LEXER LEXER LEXER LEXER RULES RULES RULES RULES RULES RULES RULES
+ */
+
+
+
--- LAND-midtermSourcecodes/target/generated-sources/antlr4/IntCalculatorBaseListener.java
+++ LAND-midtermSourcecodes/target/generated-sources/antlr4/IntCalculatorBaseListener.java
@@ -15,6 +15,330 @@ public class IntCalculatorBaseListener implements IntCalculatorListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
+	@Override public void enterData_type(IntCalculatorParser.Data_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitData_type(IntCalculatorParser.Data_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterLiteral(IntCalculatorParser.LiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitLiteral(IntCalculatorParser.LiteralContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDigit_literal(IntCalculatorParser.Digit_literalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDigit_literal(IntCalculatorParser.Digit_literalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCharacters_literal(IntCalculatorParser.Characters_literalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCharacters_literal(IntCalculatorParser.Characters_literalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterReturn_type(IntCalculatorParser.Return_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitReturn_type(IntCalculatorParser.Return_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConstant_declaration(IntCalculatorParser.Constant_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConstant_declaration(IntCalculatorParser.Constant_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVar(IntCalculatorParser.VarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVar(IntCalculatorParser.VarContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterVariable_declaration(IntCalculatorParser.Variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitVariable_declaration(IntCalculatorParser.Variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMore_variable_declaration(IntCalculatorParser.More_variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMore_variable_declaration(IntCalculatorParser.More_variable_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArray(IntCalculatorParser.ArrayContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArray(IntCalculatorParser.ArrayContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment_declaration(IntCalculatorParser.Assignment_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment_declaration(IntCalculatorParser.Assignment_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment_array(IntCalculatorParser.Assignment_arrayContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment_array(IntCalculatorParser.Assignment_arrayContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment(IntCalculatorParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment(IntCalculatorParser.AssignmentContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterAssignment_line(IntCalculatorParser.Assignment_lineContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitAssignment_line(IntCalculatorParser.Assignment_lineContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_declaration(IntCalculatorParser.Function_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_declaration(IntCalculatorParser.Function_declarationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_declaration_parameters_type(IntCalculatorParser.Function_declaration_parameters_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_declaration_parameters_type(IntCalculatorParser.Function_declaration_parameters_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_declaration_parameters(IntCalculatorParser.Function_declaration_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_declaration_parameters(IntCalculatorParser.Function_declaration_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_declaration_more_parameters(IntCalculatorParser.Function_declaration_more_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_declaration_more_parameters(IntCalculatorParser.Function_declaration_more_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_call(IntCalculatorParser.Function_callContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_call(IntCalculatorParser.Function_callContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_call_parameters_type(IntCalculatorParser.Function_call_parameters_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_call_parameters_type(IntCalculatorParser.Function_call_parameters_typeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_call_parameters(IntCalculatorParser.Function_call_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_call_parameters(IntCalculatorParser.Function_call_parametersContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFunction_call_line(IntCalculatorParser.Function_call_lineContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFunction_call_line(IntCalculatorParser.Function_call_lineContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConditional(IntCalculatorParser.ConditionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConditional(IntCalculatorParser.ConditionalContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterConditional_continue(IntCalculatorParser.Conditional_continueContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitConditional_continue(IntCalculatorParser.Conditional_continueContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWloop(IntCalculatorParser.WloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWloop(IntCalculatorParser.WloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDloop(IntCalculatorParser.DloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDloop(IntCalculatorParser.DloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFloop(IntCalculatorParser.FloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFloop(IntCalculatorParser.FloopContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
 	@Override public void enterPerform_op(IntCalculatorParser.Perform_opContext ctx) { }
 	/**
 	 * {@inheritDoc}
@@ -70,6 +394,54 @@ public class IntCalculatorBaseListener implements IntCalculatorListener {
 	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitMul_div(IntCalculatorParser.Mul_divContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCond_op(IntCalculatorParser.Cond_opContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCond_op(IntCalculatorParser.Cond_opContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterExpression(IntCalculatorParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitExpression(IntCalculatorParser.ExpressionContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterMore_expressions(IntCalculatorParser.More_expressionsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitMore_expressions(IntCalculatorParser.More_expressionsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCode_block(IntCalculatorParser.Code_blockContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCode_block(IntCalculatorParser.Code_blockContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
